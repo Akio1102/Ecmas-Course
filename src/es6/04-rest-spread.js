ecaea1390b2ae57553433b26257a0af5a701d2fd
Akio <cristian.akio1102@gmail.com>
2023-02-04 19:40:43 -0500

    Class 7

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,10 @@
+// arrays destructuring
+let fruits = ["Apple", "Banana"];
+let [a, b] = fruits;
+console.log(a, b);
+
+// Object destructuring
+
+let user = { username: "Akio", age: 20 };
+let { username, age } = user;
+console.log(username, age);
