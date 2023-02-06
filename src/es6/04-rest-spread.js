fc0f5d9d726d07d5e2e855aa1156d2fc4550ccb9
Akio <cristian.akio1102@gmail.com>
2023-02-05 22:08:45 -0500

    Class 8

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -8,3 +8,21 @@ console.log(a, b);
 let user = { username: "Akio", age: 20 };
 let { username, age } = user;
 console.log(username, age);
+
+// Spread operator
+
+let person = { name: "Akio", age: 20 };
+let country = "CO";
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// Rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3);
