bda8da7a67e54eb7a8f8738edf1dc0b62ab97506
Akio <cristian.akio1102@gmail.com>
2023-03-14 22:02:23 -0500

    Class 19

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,9 @@
+//Transforma el Objecto a un array
+const countries = { MX: "Mexico", CO: "Colombia", CL: "Chile", PE: "Peru" };
+console.log(Object.entries(countries));
+/*[
+    [ 'MX', 'Mexico' ],
+    [ 'CO', 'Colombia' ],
+    [ 'CL', 'Chile' ],
+    [ 'PE', 'Peru' ]
+]*/
---
+++ src/es8/01-object-value.js
@@ -0,0 +1,4 @@
+//Con Object.values() obtenemos solo los value del objecto convertido en Array
+//Con Object.key() obtenemos solo los key del objecto convertido en Array
+const countries = { MX: "Mexico", CO: "Colombia", CL: "Chile", PE: "Peru" };
+console.log(Object.values(countries));
