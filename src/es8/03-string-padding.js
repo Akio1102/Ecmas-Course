fba7b737f87157ed180bbde32ae6a28431b5600b
Akio <cristian.akio1102@gmail.com>
2023-03-14 22:13:46 -0500

    Class 20

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,5 @@
+const string = "Hello";
+//El metodo padStart() Rellena el string al comienzo especificandole cuan grande quiere que sea el string y con que
+console.log(string.padStart(6, "_"));
+//El metodo padEnd() Rellena el string al final especificandole cuan grande quiere que sea el string y con que
+console.log(string.padEnd(6, "_"));
---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,4 @@
+// Las Trailing Commas permite poenr comas despues de objectos o array crear items vacios
+const array = [24, 34, 25, 24, , , , 45];
+console.log(array);
+console.log(array.length);
