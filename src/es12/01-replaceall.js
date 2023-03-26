8e3c9b1694c60702606688d5c453faa6a573c8e2
Akio <cristian.akio1102@gmail.com>
2023-03-26 17:55:12 -0500

    Class 31

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,7 @@
+//El método replace recibe como parametro un string el cual vamos a modificar el problema es que este solo modifica el 1 elemento que le pasemos
+
+//El método replaceAll retorna un nuevo string, reemplazando todos los elementos por otro.
+
+const string = "JS is a language of programming";
+const replacedString = string.replace("JS", "TypeScript");
+console.log(replacedString);
