58f844235a5385bd0be0b4da03ac16949a0522ff
Akio <cristian.akio1102@gmail.com>
2023-03-26 17:12:50 -0500

    Class 29

---
+++ src/es11/04-globalthis.js
@@ -0,0 +1,4 @@
+console.log(window); // Navegador
+console.log(global); // Node
+console.log(self); // WebWorker
+console.log(globalThis); //Contexto Global, sirve en todos laos anteriores y podra servir para cualquier tipo de app con js
